f7ec627bec2f3a803fe395857365f77c733e603e
Tobita0116 <kagashow0116@gmail.com>
2019-10-03 14:41:18 +0900

    ss

--- config/deploy.rb
+++ config/deploy.rb
@@ -52,7 +52,7 @@ set :rbenv_ruby, '2.5.1'
 
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/.ssh/mustache.pem.pem']  
+                  keys: ['/.ssh/mustache.pem']  
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
